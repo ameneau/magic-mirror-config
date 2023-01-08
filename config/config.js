153eadfba2cb97b5208d0b1db52838e4d1db4f23
Arthur Meneau <arthur.meneau@gmail.com>
2023-01-07 22:05:18 -0800

    used js formatter to cleanup some tabs
    commented out the google photos module for now
    
    added the Novato openweather code

--- config/config.js
+++ config/config.js
@@ -9,18 +9,18 @@
  */
 let config = {
 	address: "localhost", 	// Address to listen on, can be:
-							// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
-							// - another specific IPv4/6 to listen on a specific interface
-							// - "0.0.0.0", "::" to listen on any interface
-							// Default, when address config is left out or empty, is "localhost"
-	port: 8080,
+	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	// - another specific IPv4/6 to listen on a specific interface
+	// - "0.0.0.0", "::" to listen on any interface
+	// Default, when address config is left out or empty, is "localhost"
+	port: 8081,
 	basePath: "/", 	// The URL path where MagicMirror² is hosted. If you are using a Reverse proxy
-					// you must set the sub path here. basePath must end with a /
+	// you must set the sub path here. basePath must end with a /
 	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], 	// Set [] to allow all IP addresses
-															// or add a specific IPv4 of 192.168.1.5 :
-															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
-															// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
-															// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
+	// or add a specific IPv4 of 192.168.1.5 :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
 	useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
 	httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
@@ -38,30 +38,30 @@ let config = {
 	// true, force serveronly mode, because you want to.. no UI on this device
 
 	modules: [
-{
-  module: "MMM-GooglePhotos",
-  position: "fullscreen",
-  config: {
-		albums: [/* INSERT YOUR OWN ALBUMS HERE! - JeffreyATW */], // Set your album name. like ["My wedding", "family share", "Travle to Paris"]
-		updateInterval: 1000 * 60, // minimum 10 seconds.
-		sort: "random", // "old", "random"
-		uploadAlbum: null, // Only album created by `create_uploadable_album.js`.
-		condition: {
-			fromDate: null, // Or "2018-03", RFC ... format available
-			toDate: null, // Or "2019-12-25",
-			minWidth: null, // Or 400
-			maxWidth: null, // Or 8000
-			minHeight: null, // Or 400
-			maxHeight: null, // Or 8000
-			minWHRatio: null,
-			maxWHRatio: null,
-			// WHRatio = Width/Height ratio ( ==1 : Squared Photo,   < 1 : Portraited Photo, > 1 : Landscaped Photo)
-		},
-		showWidth: 1080, // These values will be used for quality of downloaded photos to show. real size to show in your MagicMirror region is recommended.
-		showHeight: 1920,
-		timeFormat: "YYYY/MM/DD HH:mm", // Or `relative` can be used.
-  }
-},
+		// {
+		// 	module: "MMM-GooglePhotos",
+		// 	position: "fullscreen",
+		// 	config: {
+		// 		albums: [/* INSERT YOUR OWN ALBUMS HERE! - JeffreyATW */], // Set your album name. like ["My wedding", "family share", "Travle to Paris"]
+		// 		updateInterval: 1000 * 60, // minimum 10 seconds.
+		// 		sort: "random", // "old", "random"
+		// 		uploadAlbum: null, // Only album created by `create_uploadable_album.js`.
+		// 		condition: {
+		// 			fromDate: null, // Or "2018-03", RFC ... format available
+		// 			toDate: null, // Or "2019-12-25",
+		// 			minWidth: null, // Or 400
+		// 			maxWidth: null, // Or 8000
+		// 			minHeight: null, // Or 400
+		// 			maxHeight: null, // Or 8000
+		// 			minWHRatio: null,
+		// 			maxWHRatio: null,
+		// 			// WHRatio = Width/Height ratio ( ==1 : Squared Photo,   < 1 : Portraited Photo, > 1 : Landscaped Photo)
+		// 		},
+		// 		showWidth: 1080, // These values will be used for quality of downloaded photos to show. real size to show in your MagicMirror region is recommended.
+		// 		showHeight: 1920,
+		// 		timeFormat: "YYYY/MM/DD HH:mm", // Or `relative` can be used.
+		// 	}
+		// },
 		{
 			module: "updatenotification",
 			position: "top_bar"
@@ -70,36 +70,36 @@ let config = {
 			module: "clock",
 			position: "top_left"
 		},
-{
-  module: 'MMM-CalendarExt2',
-  config: {
-    calendars : [
-	    // INSERT YOUR OWN CALENDARS HERE! - JeffreyATW
+		{
+			module: 'MMM-CalendarExt2',
+			config: {
+				calendars: [
+					// INSERT YOUR OWN CALENDARS HERE! - JeffreyATW
 				],
-    views: [
-      {
-        mode: "daily",
-  position: "middle_center",
-	      slotCount: 7,
-		slotMaxHeight: "auto",
-timeFormat: "h:mm A"
-      },
-    ],
-    scenes: [
-      {
-        name: "DEFAULT",
-      },
-    ],
-  },
-},
+				views: [
+					{
+						mode: "daily",
+						position: "middle_center",
+						slotCount: 7,
+						slotMaxHeight: "auto",
+						timeFormat: "h:mm A"
+					},
+				],
+				scenes: [
+					{
+						name: "DEFAULT",
+					},
+				],
+			},
+		},
 		{
 			module: "weather",
 			position: "top_center",
 			config: {
 				weatherProvider: "openweathermap",
 				type: "current",
-				location: "Berkeley",
-				locationID: "5327684", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				location: "Novato",
+				locationID: "5378044", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
 				apiKey: "" // INSERT YOUR OWN API KEY HERE! - JeffreyATW
 			}
 		},
@@ -110,8 +110,8 @@ timeFormat: "h:mm A"
 			config: {
 				weatherProvider: "openweathermap",
 				type: "forecast",
-				location: "Berkeley",
-				locationID: "5327684", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				location: "Novato",
+				locationID: "5378044", //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
 				apiKey: "" // INSERT YOUR OWN API KEY HERE! - JeffreyATW
 			}
 		},
@@ -135,4 +135,4 @@ timeFormat: "h:mm A"
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {module.exports = config;}
+if (typeof module !== "undefined") { module.exports = config; }
